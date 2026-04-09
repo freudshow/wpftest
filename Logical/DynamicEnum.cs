191ee1e70d8581e5810ce23298799a8d31d03e61
s_baoshan <s_baoshan@163.com>
2026-04-09 17:41:33 +0800

    DynamicEnum

---
+++ Logical/DynamicEnum.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Reflection;
+
+public class EnumGenerator
+{
+    public static Type GenerateEnumFromTable(string connectionString, string tableName)
+    {
+        using (var connection = new SQLiteConnection(connectionString))
+        {
+            connection.Open();
+
+            var query = $"SELECT index, name FROM {tableName}";
+            using (var command = new SQLiteCommand(query, connection))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    var enumType = CreateEnumType();
+
+                    while (reader.Read())
+                    {
+                        int index = reader.GetInt32(0);
+                        string name = reader.GetString(1);
+
+                        // Check if the index is not already assigned to an enum value
+                        if (!Enum.IsDefined(enumType, index.ToString()))
+                        {
+                            // Add the new enum value
+                            AddEnumValue(enumType, index, name);
+                        }
+                    }
+                }
+            }
+        }
+
+        return enumType;
+    }
+
+    private static Type CreateEnumType()
+    {
+        var enumType = typeof(MyDynamicEnum);
+        var enumBuilder = EnumBuilder.Create(enumType);
+        return enumBuilder.Create();
+    }
+
+    private static void AddEnumValue(Type enumType, int index, string name)
+    {
+        var fieldInfo = enumType.GetField(index.ToString());
+        if (fieldInfo != null)
+        {
+            fieldInfo.SetValue(null, name);
+        }
+        else
+        {
+            var enumBuilder = EnumBuilder.Create(enumType);
+            enumBuilder.DefineEnumValue(index.ToString(), name);
+            enumBuilder.Create();
+        }
+    }
+}
+
+public class EnumBuilder
+{
+    private static readonly Lazy<Dictionary<Type, EnumBuilder>> EnumBuilders = new Lazy<Dictionary<Type, EnumBuilder>>(() =>
+    {
+        var builders = new Dictionary<Type, EnumBuilder>();
+        foreach (var type in AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()))
+        {
+            if (type.IsEnum)
+            {
+                builders[type] = new EnumBuilder(type);
+            }
+        }
+        return builders;
+    });
+
+    private readonly Type _enumType;
+
+    private EnumBuilder(Type enumType)
+    {
+        _enumType = enumType;
+    }
+
+    public static EnumBuilder Create(Type enumType)
+    {
+        return EnumBuilders.Value[_enumType];
+    }
+
+    public void DefineEnumValue(string valueName, string displayName)
+    {
+        var fieldInfo = _enumType.GetField(valueName);
+        if (fieldInfo != null)
+        {
+            fieldInfo.SetValue(null, displayName);
+        }
+        else
+        {
+            var fieldBuilder = _enumType.GetTypeInfo().DeclaredTypes.First(t => t.Name == "EnumBuilder").GetField("fields");
+            fieldBuilder.SetValue(fieldBuilder.GetValue(null), fieldBuilder.GetValue(null).GetValue(0).Add(new KeyValuePair<string, string>(valueName, displayName)));
+        }
+    }
+}
+
+public enum MyDynamicEnum
+{
+    None = 0,
+    Value1 = 1,
+    Value2 = 2,
+    Value3 = 3
+}
---
+++ ModifyAttributes/ModifyConfigFilesClass.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.ModifyAttributes
+{
+    public enum ResultTypeEnum
+    {
+        Result_Type_Invalid = -1,
+        Result_Type_Int32 = 0,
+        Result_Type_Double,
+        Result_Type_Boolean,
+        Result_Type_Positive_Infinity,
+        Result_Type_Negtive_Infinity,
+        Result_Type_Byte_Array,
+        Result_Type_String,
+    }
+
+    public enum ResultSignEnum
+    {
+        Result_Sign_Invalid = -1,
+        Result_Sign_Equal = 0,
+        Result_Sign_Greater_Than,
+        Result_Sign_Less_Than,
+        Result_Sign_Interval,
+        Result_Sign_Regex,
+        Result_Sign_Lambda,
+        Result_NotValiad
+    }
+
+    public class SftpFileTransferParameters
+    {
+        public bool IsUploadFileToTerminal = true;
+        public string FullFileNameTerminal;
+        public string FullFileNameComputer;
+    }
+
+    public class SSHCommandClass
+    {
+        public string Command { get; set; } = "";
+        public string Result { get; set; } = "";
+        public ResultSignEnum ResultSign { get; set; } = ResultSignEnum.Result_Sign_Equal;
+        public string Description { get; set; } = "";
+        public int Delay { get; set; } = 0;
+    }
+
+    public class CheckSingleSftpClass
+    {
+        public string Description { get; set; }
+        public SSHCommandClass SSHCmdBeforeDownload { get; set; }
+        public SftpFileTransferParameters SftpParam { get; set; }
+        public SSHCommandClass SSHCmdAfterDownload { get; set; }
+    }
+
+    public class AttributeValueFromControl
+    {
+    }
+
+    public class ModifyAttributeValue
+    {
+        private int ConfigType;
+        private string ConfigValue;
+    }
+
+    public class ConfigAttributeItem
+    {
+        public string ConfigName;
+        public ModifyAttributeValue ConfigValue;
+    }
+
+    public class ModifyConfigFileItem
+    {
+        public string FileName { get; set; }
+
+        public CheckSingleSftpClass DownloadToTerminal { get; set; }
+
+        public List<ConfigAttributeItem> ConfigAttributeList { get; set; }
+    }
+
+    public class ModifyConfigFilesClass
+    {
+        public List<ModifyConfigFileItem> ModifyConfigFileList { get; set; }
+    }
+}
